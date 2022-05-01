1a7f6e8a2118c03e5db154d1cb49f262b62a2f24
Cassandra Julienne Mojica <76179543+CassandraJM@users.noreply.github.com>
2022-05-01 03:40:39 -0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 #from app import *
-from main import *
+#from main import *
 from waitress import serve
 import os
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
@@ -8,3 +8,4 @@ os.environ["TOKENIZERS_PARALLELISM"] = "false"
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=8000)
     #app.run()
+from main import *
